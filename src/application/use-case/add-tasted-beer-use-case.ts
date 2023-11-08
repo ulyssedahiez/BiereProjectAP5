13bdafd650d50d435fe9dc11097531c214ca1a61
Ulysse Dahiez <dahiez.ulysse@gmail.com>
2023-11-08 15:00:41 +0100

    okok

--- src/application/use-case/add-tasted-beer-use-case.ts
+++ src/application/use-case/add-tasted-beer-use-case.ts
@@ -11,15 +11,12 @@ export type addTastedBeersDependencies = {
 export async function addTastedBeersUseCase(deps: addTastedBeersDependencies, beerd: TastedBeer): Promise<void> {
   try {
     const beers = await deps.beerRepository.getAllBeers();
-    
     const beer = beers.find((beerz) => beerz.id === beerd.id);
-   
     if (!beer) {
       throw new Error("Not found");
     }
 
     const tastedBeer = new TastedBeer({ id: beer.id, name: beer.name });
-
     return await deps.tastedBeerRepository.addTastedBeer(tastedBeer);
   } catch (err) {
     console.error(err);
